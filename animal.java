04ad9f8e2f2c172b4e8b6b0278d7266c0ddcb6c6
sagnar <sagarkarki184@gmail.com>
2019-05-18 14:38:52 +0545

    first program

---
+++ animal.java
@@ -0,0 +1,16 @@
+public class animal
+{
+	String name;
+	int weight;
+public animal()
+{
+	this.name=name;
+	this.weight=weight;
+}
+public static void main(String[] args) {
+
+System.out.println("first message to git");
+	
+}
+
+}
